2e1fc46b34014d12a03fe7c1df2ec1860c3341b5
Min <JKMIN@SOUTHERNCO.COM>
2017-06-25 17:03:53 -0400

    my solution

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("init setup");
+            string[] tokens_n = args; //Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int k = Convert.ToInt32(tokens_n[1]);
+            string[] a_temp = tokens_n[2].Split(' '); //Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+
+            int[] output = new int[n];
+            Array.Copy(a, k, output, 0, n - k);
+
+            int outputIndex = n - k;
+            for (int i = 0; i < k; i++)
+            {
+                output[outputIndex++] = a[i];
+            }
+
+            Console.WriteLine(string.Join(" ", output));
+            Console.ReadLine();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ArrayLeftRotation
+
+## Given an array of n integers and a number, d, perform  left rotations on the array. Then print the updated array as a single line of space-separated integers.
